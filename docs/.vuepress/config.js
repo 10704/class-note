2f906e195880008022397b18f45f23a03c85f449
10704 <11035092@fhsh.tp.edu.tw>
2022-03-14 17:04:36 +0800

    update 20220311 note

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,5 @@ module.exports = {
   
   theme: "@vuepress/theme-default",
   themeConfig: {
-    logo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fgw.alicdn.com%2Fi1%2F680302322%2FO1CN01w13ju61T1WGoVa75k_!!680302322.jpg_500x500Q75.jpg_.webp&imgrefurl=https%3A%2F%2Fworld.taobao.com%2Fitem%2F620329232725.htm&tbnid=J3caAyBmpae5tM&vet=10CNQBEDMolQNqFwoTCOCm8dqexfYCFQAAAAAdAAAAABAE..i&docid=4G6wz6WlazGfNM&w=500&h=500&q=%E5%8D%A1%E9%80%9A%E5%9C%96%E6%A1%88&ved=0CNQBEDMolQNqFwoTCOCm8dqexfYCFQAAAAAdAAAAABAE"
+    logo: "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.imeitou.com%2Fkatong%2F198898.html&psig=AOvVaw1sPtTjpubcLrG-DRUYEQXa&ust=1647334856048000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCm8dqexfYCFQAAAAAdAAAAABAH"
 };
