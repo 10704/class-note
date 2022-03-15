347c34c26d97316eaa9844a4720b39b66d06117c
10704 <11035092@fhsh.tp.edu.tw>
2022-03-15 12:37:48 +0800

    fixed config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   title: "基礎程式設計筆記",
   description: "This is my first vuepress site",
-  
+
   theme: "@vuepress/theme-default",
-  themeConfig: '{'
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNIC7POChLQDtzgYC5AF4ANB7dnunVzNLPMA&usqp=CAU",
+  themeConfig: {
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNIC7POChLQDtzgYC5AF4ANB7dnunVzNLPMA&usqp=CAU"
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,5 @@
 ---
 home: true
-heroImage: "http://image.biaobaiju.com/uploads/20190519/21/1558272960-JZSbtohjqE.jpg"
+heroImage: " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX9uktussDt2gg8ptTHeozwOgKyFuoZmW55A&usqp=CAU "
 heroText: 10704的基礎程式筆記
 ---
